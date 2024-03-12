699b9375523e805509a93544a93054f7074e0a3e
Kothore <100525076+KothoreBornalex@users.noreply.github.com>
2024-03-12 10:38:45 +0100

    Commit Upgrade Computer + Upgrade EntityTrigger

--- Assets/_Project/Scripts/Ibrahim/System_Camera/CameraDetector.cs
+++ Assets/_Project/Scripts/Ibrahim/System_Camera/CameraDetector.cs
@@ -5,13 +5,14 @@ using UnityEngine.Rendering.Universal;
 
 public class CameraDetector : Objective
 {
-    [SerializeField] private Light2D _light;
-    [SerializeField] private SpotPlayer _detector;
 
-    public void Desactivate()
+    public void SetObjectiveCompleted()
     {
         IsCompleted = true;
-        _light.intensity = 0.3f;
-        _detector.enabled = false;
+    }
+
+    public void SetObjectiveUnCompleted()
+    {
+        IsCompleted = false;
     }
 }
