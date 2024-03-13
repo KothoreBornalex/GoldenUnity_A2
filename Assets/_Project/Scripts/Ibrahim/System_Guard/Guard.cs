0921598134f26a094754247add08dc4499ab2036
Indra <armand.ribadeau_dumas@edu.devinci.fr>
2024-03-13 16:49:25 +0100

    Update Guard.cs

--- Assets/_Project/Scripts/Ibrahim/System_Guard/Guard.cs
+++ Assets/_Project/Scripts/Ibrahim/System_Guard/Guard.cs
@@ -45,7 +45,7 @@ public class Guard : Objective
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.up);
+        Quaternion targetRotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 3.0f * Time.deltaTime);
         
     }
