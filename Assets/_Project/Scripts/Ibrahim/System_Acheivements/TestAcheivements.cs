483da89ff5c3541409c864ff5cda3755d0ae5e5b
Ibrahim-Mallouka <panda.bornalex@gmail.com>
2024-03-14 15:21:46 +0100

    Commit Android Fix

---
+++ Assets/_Project/Scripts/Ibrahim/System_Acheivements/TestAcheivements.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAcheivements : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AcheivementsManager.instance.UnlockSuccess(GameAssets.instance.AchievementsBank.AmogUs_ID);
+    }
+
+
+}
