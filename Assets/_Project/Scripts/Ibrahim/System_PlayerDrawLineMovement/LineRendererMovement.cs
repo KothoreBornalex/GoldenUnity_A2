18ed7ad21facc3b85e12424f6b536df87059433f
Indra <armand.ribadeau_dumas@edu.devinci.fr>
2024-03-15 12:54:41 +0100

    fix sound

--- Assets/_Project/Scripts/Ibrahim/System_PlayerDrawLineMovement/LineRendererMovement.cs
+++ Assets/_Project/Scripts/Ibrahim/System_PlayerDrawLineMovement/LineRendererMovement.cs
@@ -125,7 +125,7 @@ public class LineRendererMovement : MonoBehaviour
         if(_isMoving)
         {
             
-            //SoundManager.Instance.PlaySound(GameAssets.instance.SoundBank._grandpaMove);
+            SoundManager.Instance.PlaySound(GameAssets.instance.SoundBank._grandpaMove);
             
             if (_linePath.positionCount != 0)
             {
@@ -402,7 +402,7 @@ public class LineRendererMovement : MonoBehaviour
 
     private IEnumerator DeletePath()
     {
-        SoundManager.Instance.PlaySound(GameAssets.instance.SoundBank._cancelPath);
+        
         int deletedVert = 0;
         float currentTimer = 0;
         float timing = 1.0f / (_resetPathSpeed * 10.0f);
